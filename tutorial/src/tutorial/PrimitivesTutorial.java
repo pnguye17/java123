61c5d1be7464d9fd2adf660041e01b61f88236ab
pnguye17 <peter.nguyen17@gmail.com>
2023-07-30 00:09:52 -0700

    primitives

---
+++ tutorial/src/tutorial/PrimitivesTutorial.java
@@ -0,0 +1,25 @@
+package tutorial;
+
+public class PrimitivesTutorial {
+
+	public static void main(String[] args) {
+		//primitves
+		
+		boolean isStudent = true;
+		char letter = 'a';
+		
+		byte b = 1;
+		short s = 1;
+		int i = 1;
+		long l = 1L;
+		
+		float f = 1f;
+		double d = 1d;
+		
+		
+		
+		
+
+	}
+
+}
