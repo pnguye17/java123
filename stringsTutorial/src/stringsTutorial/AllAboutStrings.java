da0d5361daffa2a9445d34244d843dba77bbf624
pnguye17 <peter.nguyen17@gmail.com>
2023-07-30 23:36:02 -0700

    stringbuilder vs stringbuffer

--- stringsTutorial/src/stringsTutorial/AllAboutStrings.java
+++ stringsTutorial/src/stringsTutorial/AllAboutStrings.java
@@ -3,8 +3,36 @@ package stringsTutorial;
 public class AllAboutStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String basicGreeting = "Hello World, string class is meant for single thread app bc it is not thread safe";
+		
+		basicGreeting.toUpperCase();//cannot modify the existing string
+		//basicGreeting.charAt(0) = "T"
+		
+		//must creating another string for the modications
+		
+		String newString = basicGreeting.toUpperCase();
+		
+		System.out.println(newString);
+		
+		
+		//StringBuilder and StringBuffer
+		
+		StringBuilder s1 = new StringBuilder(basicGreeting);
+		StringBuffer s2 = new StringBuffer(newString);
+		
+		//we can modify using these two classes
+		
+		s1.reverse().append(s2);
+		
+		System.out.println(s2.toString());
+		System.out.print(s1.toString());
+		System.out.print(s1);
+	
+		//stringbuilder is faster but not thread safe
+		//stringbuffer is slower but is thread safe
+		
+		
+	
 	}
 
 }
