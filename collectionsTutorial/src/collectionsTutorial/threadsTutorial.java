03dafcad76d85f51b7d8fc049fce087d18515c89
pnguye17 <peter.nguyen17@gmail.com>
2023-08-01 00:21:38 -0700

    threads

--- collectionsTutorial/src/collectionsTutorial/threadsTutorial.java
+++ collectionsTutorial/src/collectionsTutorial/threadsTutorial.java
@@ -1,9 +1,61 @@
 package collectionsTutorial;
 
-public class threadsTutorial {
+//public class threadsTutorial extends Thread {
+//	
+//	//if extending Thread class, must ovveride run()
+//	
+//	public void run() {
+//		System.out.println("running");
+//	}
+//
+//	public static void main(String[] args) {
+//		//allows a program to operate more efficiently by doing multiple things at the same time.
+//		
+//		//extends the thread class
+//		
+//		threadsTutorial thread = new threadsTutorial();
+//		thread.start();
+//		
+//		
+//		
+//		
+//
+//	}
+//
+//}
+
+
+public class threadsTutorial implements Runnable {
+	
+	//if extending Thread class, must ovveride run()
+	
+	public void run() {
+		System.out.println("running");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//allows a program to operate more efficiently by doing multiple things at the same time.
+		
+		//implements the runnable interface
+		
+		threadsTutorial obj = new threadsTutorial();
+		
+		Thread thread = new Thread(obj);
+		thread.start();
+		
+		//
+		Thread t = new Thread() {
+			
+			public void run() {
+				System.out.println("running more");
+			}
+		};
+		t.start();
+		
+		
+		
+		
+		
 
 	}
 
